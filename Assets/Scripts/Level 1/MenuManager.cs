55f02f182407d0983421e09a8a1a4b7960b769e3
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2022-02-04 00:06:12 +0300

    6

--- Assets/Scripts/Level 1/MenuManager.cs
+++ Assets/Scripts/Level 1/MenuManager.cs
@@ -6,34 +6,26 @@ using Cinemachine;
 
 public class MenuManager : MonoBehaviour
 {
-    [Header("Setup Variables")]
-
+    [Header("SETUP VARIABLES")]
     [SerializeField] GameObject playButtonObject;
 
     [SerializeField] GameObject enemy1Object;
     [SerializeField] GameObject enemy2Object;
     [SerializeField] GameObject enemy3Object;
 
+    private bool moved = false;
+
     [SerializeField] GameObject gateCam;
     [SerializeField] Vector3 dollyTargetPos = new Vector3(30, 4.5f, 0);
     [SerializeField] float gateCamSpeed;
     [SerializeField] GameObject gameCam;
 
-    private bool doorBool;
+    private bool doorBool = false;
     [SerializeField] GameObject doorR;
     [SerializeField] GameObject doorL;
-    [SerializeField] float doorSpeed = 3;
-
     
-
-
-
-    
-
     public void PlayButton()
     {
-
-
         var enemy1GatePos = new Vector3(42, 14.95f, 3);
         var enemy2GatePos = new Vector3(42, 15, -0.2f);
         var enemy3GatePos = new Vector3(42.4f, 15, -3.3f);
@@ -45,38 +37,45 @@ public class MenuManager : MonoBehaviour
         playButtonObject.SetActive(false);
 
         StartCoroutine("doorTimer");
-
-        //enemy1 gate pos: 42, 14.95, 3 - enemy2 gate pos: 42, 15, -0.2 - enemy3 gate pos: 42.4, 15, -3.3
-        //enemy1 board pos: 6, 2 , 6 - enemy2 board pos: 10, 2 , 2 - enemy3 board pos: 14, 2, -2
-
-        //enemies walk up so their shadows cast on the gate
-        //the gate opens
-        //camera changes
-        // they walk to their board positions.
     }
 
     private void Update()
     {
-        if (gateCam.transform.position == dollyTargetPos)
+        if (gateCam.transform.position == dollyTargetPos && !moved)
         {
+            var enemy1BoardPos = new Vector3(6, 2, 6);
+            var enemy2BoardPos = new Vector3(10, 2, 2);
+            var enemy3BoardPos = new Vector3(14, 2, -2);
+
+            if(!moved)
+            {
+                enemy1Object.GetComponent<NavMeshAgent>().Warp(new Vector3(29, 2, 3));
+                enemy2Object.GetComponent<NavMeshAgent>().Warp(new Vector3(29, 2, 0));
+                enemy3Object.GetComponent<NavMeshAgent>().Warp(new Vector3(29, 2, -3));
+                
+                moved = true;
+            }
+            
             gameCam.GetComponent<CinemachineVirtualCamera>().Priority = 11;
-        }
-        else if (doorR.transform.rotation == Quaternion.Euler(0, 90, 0) && doorL.transform.rotation == Quaternion.Euler(0, -90, 0))
-        {
+
+            enemy1Object.GetComponent<NavMeshAgent>().SetDestination(enemy1BoardPos);
+            enemy2Object.GetComponent<NavMeshAgent>().SetDestination(enemy2BoardPos);
+            enemy3Object.GetComponent<NavMeshAgent>().SetDestination(enemy3BoardPos);
             doorBool = false;
-            gateCam.transform.position = Vector3.MoveTowards(gateCam.transform.position, dollyTargetPos, gateCamSpeed * Time.deltaTime);
         }
-        else if (doorBool && doorL.transform.rotation.y != -90 && doorL.transform.rotation.y != 90)
+       
+        else if (doorBool)
         {
-            doorR.transform.Rotate(0, 15 * Time.deltaTime, 0);
-            doorL.transform.Rotate(0, -15 * Time.deltaTime, 0);
+            doorR.transform.Rotate(0, 30 * Time.deltaTime, 0);
+            doorL.transform.Rotate(0, -30 * Time.deltaTime, 0);
+            gateCam.transform.position = Vector3.MoveTowards(gateCam.transform.position, dollyTargetPos, gateCamSpeed * Time.deltaTime);
         }
         else return;
     }
 
     IEnumerator doorTimer()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3);
         doorBool = true;
     }
 }
