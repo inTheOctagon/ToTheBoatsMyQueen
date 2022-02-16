5e7568be05dd2cc6bbb56e62a789dcc62d94e74f
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2022-02-17 00:36:38 +0300

    27

--- Assets/Scripts/Level 2/CorridorGuard.cs
+++ Assets/Scripts/Level 2/CorridorGuard.cs
@@ -4,48 +4,144 @@ using UnityEngine;
 
 public class CorridorGuard : MonoBehaviour
 {
-    [SerializeField] float moveRange;
-    [SerializeField] float moveTime;
+    [Header("Guard Variables")]
+    [SerializeField] float sidewardRange;
+    [SerializeField] float sidewardTime;
+    [SerializeField] float forwardTime;
+    private float forwardVelocity;
+    [SerializeField] float forwardMaxVelocity;
+    [Header("Shield Variables")]
+    [SerializeField] GameObject shieldObject;
+    [SerializeField] float rotatingTime;
+
+    
+
+    
 
     private bool ready = true;
 
     private void Start()
     {
+
+        StartCoroutine(Accelerate());
         
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Horizontal"))
+        if (Input.GetButtonDown("Horizontal") && ready)
         {
-            if(ready) StartCoroutine(ChangeLane());
+            StartCoroutine(ChangeLane());
+            
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space) && ready)
+        {
+            StartCoroutine(RotateShield());
         }
 
+        MoveForward();
     }
+
    
+    void MoveForward()
+    {
+        transform.Translate(Vector3.forward * forwardVelocity * Time.deltaTime);
+    }
+    
+    IEnumerator Accelerate()
+    {
+        float currentTime = Time.time;
+        var currentVelocity = 0;
+
+        while(Time.time < currentTime + forwardTime)
+        {
+            forwardVelocity = Mathf.Lerp(currentVelocity, forwardMaxVelocity, (Time.time - currentTime) / forwardTime);
+            Debug.Log(forwardVelocity);
+            yield return null;
+        }
+        
+        forwardVelocity = forwardMaxVelocity;
+    }
+
     IEnumerator ChangeLane()
     {
         ready = false;
 
         var inputValue = Input.GetAxisRaw("Horizontal");
-        Vector3 startPos = transform.position;
-        float targetPosX = startPos.x + moveRange * inputValue;
-        Vector3 targetPos = new Vector3(targetPosX, startPos.y, startPos.z);  
+        float currentPosX = transform.position.x;
+        float targetPosX = currentPosX + sidewardRange * inputValue;
+        Vector3 targetPos = new Vector3(targetPosX, transform.position.y, transform.position.z);
         
-        if(targetPosX >= -moveRange && targetPosX <= moveRange)
+        
+        if(targetPosX >= -sidewardRange && targetPosX <= sidewardRange)
 
         {
             float startTime = Time.time;
 
-            while (Time.time < startTime + moveTime)
+            while (Time.time < startTime + sidewardTime)
             {
-                transform.position = Vector3.Lerp(startPos, targetPos, (Time.time - startTime) / moveTime);
+
+                transform.position = new Vector3(Mathf.Lerp(currentPosX, targetPosX, (Time.time - startTime) / sidewardTime), transform.position.y,transform.position.z); 
+                
                 yield return null;
             }
-            transform.position = targetPos;
+            transform.position = new Vector3(targetPos.x, transform.position.y, transform.position.z);
         }
 
         ready = true;
     }
+
+    IEnumerator RotateShield()
+    {
+        ready = false;
+
+        float startTime = Time.time;
+        //shield pos variables
+        float currentPosY = shieldObject.transform.position.y;
+        float shieldPosOffset = 1.5f;
+        //shield rotation variables
+        Quaternion currentAngle = shieldObject.transform.rotation;
+        Quaternion horizontalAngle = Quaternion.Euler(0, 0, 90);
+        Quaternion verticalAngle = Quaternion.Euler(0, 0, 0);
+
+
+        if(currentAngle.eulerAngles.z == 90)
+        {
+            float targetPosY = currentPosY + shieldPosOffset;
+
+            while (Time.time < startTime + rotatingTime)
+            {
+                shieldObject.transform.position = new Vector3(shieldObject.transform.position.x, Mathf.Lerp(currentPosY, targetPosY, (Time.time - startTime) / rotatingTime), shieldObject.transform.position.z);
+                shieldObject.transform.localRotation = Quaternion.Lerp(currentAngle, verticalAngle, (Time.time - startTime) / rotatingTime);
+                yield return null;
+            }
+            shieldObject.transform.position = new Vector3(shieldObject.transform.position.x, targetPosY, shieldObject.transform.position.z);
+            shieldObject.transform.rotation = verticalAngle;
+
+            ready = true;
+
+            Debug.Log("vertical");
+        }
+        else if(currentAngle.z == 0)
+        {
+            float targetPosY = currentPosY - shieldPosOffset;
+
+            while (Time.time < startTime + rotatingTime)
+            {
+                shieldObject.transform.position = new Vector3(shieldObject.transform.position.x, Mathf.Lerp(currentPosY, targetPosY, (Time.time - startTime) / rotatingTime), shieldObject.transform.position.z);
+                shieldObject.transform.localRotation = Quaternion.Lerp(currentAngle, horizontalAngle, (Time.time - startTime) / rotatingTime);
+                yield return null;
+            }
+            shieldObject.transform.position = shieldObject.transform.position = new Vector3(shieldObject.transform.position.x, targetPosY, shieldObject.transform.position.z); ;
+            shieldObject.transform.rotation = horizontalAngle;
+
+            ready = true;
+
+            Debug.Log("horiztonal");
+        }
+        
+    }
+    
 
 }
